bf7b61b442431a8b614724321f95794e10ebde5e
Kyle Kennedy <tinkernaut26@gmail.com>
2023-11-30 14:23:55 -0800

    Updated landing page and music content

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,12 @@
                 <span v-html="WHAT_CONTENT"></span>
                 <span v-html="WHY_CONTENT"></span>
                 <span v-html="HOW_CONTENT"></span>
+                <nuxt-link to="https://github.com/sockleblu/digital_garden_frontend">DigiDen Frontend GitHub</nuxt-link>
+                <br><br>
+                <nuxt-link to="https://github.com/sockleblu/digital_garden_backend">DigiDen Backend GitHub</nuxt-link>
+                <br><br>
+                <span v-html="IAC_CONTENT"></span>
+                <br><br>
                 <nuxt-link to="/an_unexpected_journey" >An Unexpected Journey</nuxt-link>
               </div>
             </div>
@@ -28,9 +34,11 @@
 <script setup>
 const WHAT_CONTENT = "A digital garden is a personal part of the internet where we can share our interests with the world. The term stems from Mark Bernstein's use of the term 'Hypertext Garden' in 1998.<br><br>"
 
-const WHY_CONTENT = "I became uninterested in social media several years ago and have since disable all my accounts on many sites. Building this site gives me a place to share my ... but also allows me to aggregate the experience I might have from other sites as well. I hope to be able to visit my sight like I might open a newspaper each day and have all the information I'm looking for currated to my wants and needs.<br><br>"
+const WHY_CONTENT = "Having become uninterested in the experience of social media several years ago that felt forced and devisive I have set out to create my own space where I can cultivate an individual experience and share with others. My intent is for each page of this site to have a personal feel that is molded by the topic. Travel having a map of places visited and places yet to explore. Music being a place where I can learn and sharpen my skills while also sharing content and the journey itself. Books being a shelf of what I've read and want to read still, likely integrating with my Goodreads account. In the end I'd like a page that I want to visit daily, with updates from feeds and other content creators.<br><br>"
 
-const HOW_CONTENT = "For more information on the making of this site as well I'm documenting the evolution of it in the link below.<br><br>"
+const HOW_CONTENT = "Ultimately each page will likely have a space where I'm sharing something, and with that comes the blog post functionality, which is what I'm focusing on first. As this site is built top-to-bottom by myself, and I'm learning a lot about full stack engineering and new languages and frameworks, I also want to share my experience of learning how to create this very same site. I think it will be cool to look back and watch it evolve as I learn and grow with it.<br><br>To follow along I'm sharing the code on GitHub below. You can also follow the blog post series starting with the very first post at the bottom. Thanks for visiting my site!<br><br>"
+
+const IAC_CONTENT = "The infrastructure for this site is built entirely by code, which I'll share here soon as well. I leverage a combination of CINC (FOSS version of Chef), Vagrant, and GitLab Pipelines, though my plan is to move this to a cloud native architecture and document the process on this site.<br><br>I've dubbed the project DigiDen - short for Digital Garden - and you can follow the series of posts on the Programming page, or you can visit the very first post below."
 </script>
 
 <style scoped>
--- pages/music.vue
+++ pages/music.vue
@@ -1,6 +1,26 @@
 <template>
-    <main>
-      <Main></Main>
-      <BlogPostList :tags="['music']"></BlogPostList>
-    </main>
-  </template>
+  <main>
+    <Main>
+        <template v-slot:default>Mandolin and Rhythm</template>
+        <template v-slot:subtitle></template>
+    </Main>
+    <div class="container">
+      <section class="articles">
+        <div class="column is-8 is-offset-2">
+          <section class="blog-post-card card article">
+            <div class="card-content">
+              <div class="content article-body is-size-5">
+                <h1>Plans for this page</h1>
+                <span v-html="CONTENT"></span>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script setup>
+const CONTENT = "Growing up playing percussion taught me a lot about rhythm, but not much about reading and playing notes. As an adult I've picked up the mandolin, and would like to share resources I've come across and music I play here, as well as create some tools to help me learn. Namely flashcards to help memorize the fretboard and a metronome. I'm sure I'll find some other things handy to build and integrate as I learn. Anyway, this will be a place to share that journey as well.<br><br>"
+</script>
