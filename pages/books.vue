de9afe0c48f6babce5d47ba7aec1c24501e48278
Kyle Kennedy <tinkernaut26@gmail.com>
2023-12-06 12:33:38 -0800

    Updated book page content and scuba video link

--- pages/books.vue
+++ pages/books.vue
@@ -22,5 +22,5 @@
 </template>
 
 <script setup>
-const CONTENT = "Growing up playing percussion taught me a lot about rhythm, but not much about reading and playing notes. As an adult I've picked up the mandolin, and would like to share resources I've come across and music I play here, as well as create some tools to help me learn. Namely flashcards to help memorize the fretboard and a metronome. I'm sure I'll find some other things handy to build and integrate as I learn. Anyway, this will be a place to share that journey as well.<br><br>"
+const CONTENT = "As an avid reader I like to keep track of what books I want to read next, and for some reason I'm not into reviewing books for others viewing like on Goodreads, but maybe I'll be more inclined to write about books as I read them here. It'd be fun to digest them as I read too or maybe pull in some insight from other people too. I don't normally re-read books, but I've picked up Game of Thrones again (in anticipation of book 6, which will definitely drop soon...right?), and it's been fun to muse over the little tidbits and hints that are dropped so very early in the story from what we know now. Might be posting here could help me digest the Malazar series too. I'd need all the help I can get."
 </script>
--- pages/scuba.vue
+++ pages/scuba.vue
@@ -16,7 +16,7 @@
                 <iframe 
                   width="560" 
                   height="315" 
-                  src="https://www.youtube.com/embed/Go3-ir1CAtk?si=Vb_RYB3VjMVdn6w0" 
+                  src="https://www.youtube.com/embed/aEKjQK-jWtQ?si=0B1GYLXyacN3Z1hE" 
                   title="YouTube video player"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -29,9 +29,10 @@
         </div>
       </section>
     </div>
+    <BlogPostList :tags="['scuba']"></BlogPostList>
   </main>
 </template>
 
 <script setup>
-const CONTENT = "For some reason I've always been interested in scuba diving. I originally got certified for Open Water when I was 13 after winning a silent auction using all my christmas money I've saved up. Since no one else in my family did scuba, and I couldn't afford to...well do anything, much less the gear to go myself, so I didn't go for many years after getting certified.<br><br>When I left to backpack in Southeast Asia I was determined to get certified again and go diving in new places while traveling. Well, I got certified, but I wasn't ready for the feeling of reconnecting with something I didn't know I missed so much. For four months I bounced back and forth from an island in Thailand named Koh Tao - or Turtle Island. It's famous for it's incredible number of scuba centers, and for being really cheap to get certified. After my Open Water certification, well of course I needed to take the Advanced Open course too. And from there, well the tide went out and drew me with it. I took my Rescue course and came back after a border run to start the Dive Master program - completing what the instructors would refer to as going 'Zero-to-Hero' - going all the way from open water to becoming a dive master.<br><br>I spent most of my time and my savings getting it, but it's a chapter of my life that was so natural it felt necessary, and I'd do it again. Here will be a place where I can share and grow that passion with others. Here's a little tase of some of the things I've seen."
+const CONTENT = "For some reason I've always been interested in scuba diving. I originally got certified for Open Water when I was 13 after winning a silent auction using all my christmas money I've saved up. Since no one else in my family did scuba, and I couldn't afford to...well do anything, much less the gear to go myself, so I didn't go for many years after getting certified.<br><br>When I left to backpack in Southeast Asia I was determined to get certified again and go diving in new places while traveling. Well, I got certified, but I wasn't ready for the feeling of reconnecting with something I didn't know I missed so much. For four months I bounced back and forth from an island in Thailand named Koh Tao - or Turtle Island. It's famous for it's incredible number of scuba centers, and for being really cheap to get certified. After my Open Water certification, well of course I needed to take the Advanced Open course too. And from there, well the tide went out and drew me with it. I took my Rescue course and came back after a border run to start the Dive Master program - completing what the instructors would refer to as going 'Zero-to-Hero' - going all the way from open water to becoming a dive master.<br><br>I spent most of my time and my savings getting it, but it's a chapter of my life that was so natural it felt necessary, and I'd do it again. Here will be a place where I can share and grow that passion with others. Here's a little taste of some of the things I've seen."
 </script>
